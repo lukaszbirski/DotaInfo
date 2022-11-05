c28c3b1ef0e04d7ee17a056ebf9f03f9358e2b81
Lukasz <lukasz.birski@gmail.com>
2022-11-05 21:31:51 +0100

    Added app center and build types

---
+++ buildSrc/src/main/java/AppCenter.kt
@@ -0,0 +1,5 @@
+object AppCenter {
+    private const val version = "4.4.5"
+    const val analytics = "com.microsoft.appcenter:appcenter-analytics:$version"
+    const val crashes = "com.microsoft.appcenter:appcenter-crashes:$version"
+}
