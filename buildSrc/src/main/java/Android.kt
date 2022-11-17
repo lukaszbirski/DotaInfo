a916c260a896a80a05fb13a37ab509d14aa7e1d9
Lukasz <lukasz.birski@gmail.com>
2022-11-18 00:03:39 +0100

    Updated versionCode and versionName to "1.1" (2)

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -4,6 +4,6 @@ object Android {
     const val buildTools = "30.0.3"
     const val minSdk = 21
     const val targetSdk = 30
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
 }
