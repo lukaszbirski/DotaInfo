f192642653f9108e7e57e9072a6c1a14cd8129fe
INFORMICA\lukasz.birski <lukasz.birski@informica.pl>
2024-01-02 23:26:23 +0100

    build.gradle Files

--- hero/ui-heroDetail/build.gradle.kts
+++ hero/ui-heroDetail/build.gradle.kts
@@ -1,3 +1,7 @@
+apply {
+    from("$rootDir/android-library-build.gradle")
+}
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -5,36 +9,7 @@ plugins {
 
 android {
     namespace = "pl.birskidev.ui_herodetail"
-    compileSdk = Android.compileSdk
-
-    defaultConfig {
-        minSdk = Android.minSdk
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-
-    implementation(AndroidX.coreKtx)
-    implementation(AndroidX.appCompat)
-    implementation(Google.material)
-    testImplementation(Junit.junit4)
-    androidTestImplementation(Junit.extJunit4)
-    androidTestImplementation(Espresso.espresso)
 }
