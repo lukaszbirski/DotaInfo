1541bfa3c5022e1f27b303dd2a3f1bd808c7c077
INFORMICA\lukasz.birski <lukasz.birski@informica.pl>
2024-01-02 23:42:28 +0100

    Hero Domain Model

---
+++ hero/hero-domain/src/main/java/pl/birskidev/hero_domain/HeroAttackType.kt
@@ -0,0 +1,24 @@
+package pl.birskidev.hero_domain
+
+sealed class HeroAttackType(
+    val uiValue: String
+) {
+
+    object Melee : HeroAttackType(
+        uiValue = "Melee",
+    )
+
+    object Ranged : HeroAttackType(
+        uiValue = "Ranged",
+    )
+
+    object Unknown : HeroAttackType(
+        uiValue = "Unknown",
+    )
+}
+
+fun getHeroAttackType(uiValue: String) = when (uiValue) {
+        HeroAttackType.Melee.uiValue -> HeroAttackType.Melee
+        HeroAttackType.Ranged.uiValue -> HeroAttackType.Ranged
+        else -> HeroAttackType.Unknown
+    }
