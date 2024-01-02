804a3de5cd04a75bc336cb9cdbb660503501a85f
INFORMICA\lukasz.birski <lukasz.birski@informica.pl>
2024-01-02 22:09:14 +0100

    Initial commit

---
+++ app/src/main/java/pl/birskidev/dotainfo/MainActivity.kt
@@ -0,0 +1,42 @@
+package pl.birskidev.dotainfo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pl.birskidev.dotainfo.ui.theme.DotaInfoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DotaInfoTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DotaInfoTheme {
+        Greeting("Android")
+    }
+}
